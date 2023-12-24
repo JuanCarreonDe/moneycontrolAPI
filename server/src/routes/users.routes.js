55688fa2106b32436d1079c5d63386b2fff94de8
Juan Carreon <105535926+JuanCarreonDe@users.noreply.github.com>
2023-12-24 17:12:49 -0600

    Delete server directory

--- server/src/routes/users.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express'
-import { getUsers, getUserBalance } from '../controllers/users.controller.js';
-const router = Router()
-
-router.get("/userBalance/:idUser", getUserBalance);
-
-router.get("/bd", getUsers);
-
-export default router
